b21e8b3d9ebd51054af9e5ecf7cd11c75ffc774e
Sakuna Dewanarayana <68386969+sakunadew99@users.noreply.github.com>
2024-06-19 13:24:46 +0530

    modify footer

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { registerUser, loginUser, getUserProfile } = require('../controllers/authController');
+const { registerUser, loginUser, getUserProfile, updateUserProfile } = require('../controllers/authController');
 const { protect, admin } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
-router.route('/profile').get(protect, getUserProfile);
+router.route('/profile')
+    .get(protect, getUserProfile)   // Existing route to get user profile
+    .put(protect, updateUserProfile); // New route to update user profile
 
 module.exports = router;
